b9388f06dd679bf3214a43f1da2cdc4564bf0152
abdulhai <abdulhai@mit.edu>
2020-10-16 12:14:18 -0400

    fixed git ignore

---
+++ agent_utils/utils.py
@@ -0,0 +1,27 @@
+import logging
+
+
+def set_logger(logger_name, log_file, level=logging.INFO):
+    log = logging.getLogger(logger_name)
+    formatter = logging.Formatter('%(asctime)s : %(message)s')
+    fileHandler = logging.FileHandler(log_file, mode='w')
+    fileHandler.setFormatter(formatter)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+
+    log.setLevel(level)
+    log.addHandler(fileHandler)
+    log.addHandler(streamHandler)
+
+
+def set_log(args):
+    log = {}
+    set_logger(
+        logger_name=args.log_name,
+        log_file=r'{0}{1}'.format("./logs/", args.log_name))
+    log[args.log_name] = logging.getLogger(args.log_name)
+
+    for arg, value in sorted(vars(args).items()):
+        log[args.log_name].info("%s: %r", arg, value)
+
+    return log
